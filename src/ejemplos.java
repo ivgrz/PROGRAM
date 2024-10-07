26898b8a66a7e48b60eae92b0a79f07dee71ad6c
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-07 13:55:59 +0200

    ej1

--- src/ejemplos.java
+++ src/ejemplos.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class ejemplos {
     public static void main (String [] args) {
 
-        int grade = 85;
+  /*      int grade = 85;
 
         if (grade > 88) {
             System.out.println("you made the honor roll");
         }
          else {
                 System.out.println("Pasaste");
+*/
 
+//Boletin3, ej 1
 
+    int x;
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        System.out.println("introduce un numero:");
+        x = (int) entradaTeclado.nextDouble();
+
+    if (x >= 0);
+        {
+            System.out.println("es positivo");
         }
+
+
+
     }
 } //end method main
