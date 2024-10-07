32313756cf28407d23de649d88b8436b3c7dd821
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-07 13:18:47 +0200

    guardado 1

---
+++ src/ejemplos.java
@@ -0,0 +1,15 @@
+public class ejemplos {
+    public static void main (String [] args) {
+
+        int grade = 85;
+
+        if (grade > 88) {
+            System.out.println("you made the honor roll");
+        }
+         else {
+                System.out.println("Pasaste");
+
+
+        }
+    }
+}
