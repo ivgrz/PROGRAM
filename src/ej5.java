66fc7342de53d0e618eaabe4ddbe98b508aab694
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-09 10:59:58 +0200

    ej5

---
+++ src/ej5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ej5 {
+    public static void main(String[] args) {
+        int x, y, z;
+
+        Scanner entradaTec = new Scanner(System.in);
+        System.out.println("introduce el primer numero: ");
+        x = entradaTec.nextInt();
+
+
+        System.out.println("introduce un segundo numero: ");
+        y = entradaTec.nextInt();
+
+
+        System.out.println("introduce un tercer numero: ");
+        z = entradaTec.nextInt();
+
+        if ()
+    }
+}
