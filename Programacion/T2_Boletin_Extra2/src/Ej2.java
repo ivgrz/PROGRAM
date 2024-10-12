a486cae19821e6a20bc5fc5bf7f009d008983c1d
DanielRTato <drodrigueztato@danielcastelao.org>
2024-10-13 01:17:21 +0200

    adfbvv2

---
+++ Programacion/T2_Boletin_Extra2/src/Ej2.java
@@ -0,0 +1,18 @@
+import  java.util.Scanner;
+public class Ej2 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        double temperaturaCentigrado, tFahrenheit, tKelvin;
+
+        System.out.println("Introduce la temperatura en grados centigrados");
+        temperaturaCentigrado = teclado.nextDouble();
+
+        tFahrenheit = (((temperaturaCentigrado * 9) /5) + 32);
+        tKelvin = temperaturaCentigrado + (273.15);
+
+        System.out.println(temperaturaCentigrado + " es equivalente a " + tFahrenheit + " en Fahrenheit y a " + tKelvin + " en Kelvin");
+
+
+    }
+}
