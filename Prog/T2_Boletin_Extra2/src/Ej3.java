2450fb4b16c024171ef3310e2bfb9f8167fce43d
DanielRTato <drodrigueztato@danielcastelao.org>
2024-10-13 17:08:00 +0200

    todo declarado

--- Prog/T2_Boletin_Extra2/src/Ej3.java
+++ Prog/T2_Boletin_Extra2/src/Ej3.java
@@ -3,7 +3,20 @@ public class Ej3 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("test");
+        System.out.println("Introduce los billetes de 100");
+        int b100 = teclado.nextInt(); //son los billetes de 100
+
+        System.out.println("Introduce los billetes de 20");
+        int b20 = teclado.nextInt(); // son los
+
+        System.out.println("Introduce los billetes de 5");
+        int b5 = teclado.nextInt();
+
+        System.out.println("Introduce las monedas de 1€");
+        int m1 = teclado.nextInt();
+
+
+
 
 
 
