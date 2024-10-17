1481c5caa3c84195ed7dc4829ec3d83cfb75e488
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-17 14:31:49 +0200

    Boletin4(3)

---
+++ src/boletin4_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class boletin4_3 {
+    public static void main(String[] args) {
+        int x, v;
+        Scanner et = new Scanner(System.in);
+        System.out.println("Pon un numero sjafhas");
+        x =et. nextInt();
+
+        if(x<0) {
+            v = (-1 * x);
+            System.out.println("el valor absoluto de tu numero es " + v);
+        }
+        else {
+            System.out.println("El valor absoluto de tu numero es " + x);
+        }
+    }
+}
