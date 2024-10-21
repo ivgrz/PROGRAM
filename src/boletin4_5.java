386de77388c9258adf7e9a29e95580227d6b6a5b
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-21 14:35:11 +0200

    ejercicio4_5

---
+++ src/boletin4_5.java
@@ -0,0 +1,16 @@
+/**
+ * Cuenta atras primer ejemplo (bucle)
+ * @version 1.1
+ * @author ivan
+ */
+public class boletin4_5 {
+    public static void main(String[] args) {
+        System.out.println("Cuenta atras: ");
+        int i;
+    for (i = 10; i >= 0; i--){
+        System.out.println(i);
+
+        }
+        System.out.println("BOOM");
+    }
+}
