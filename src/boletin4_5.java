5ef57cd9a0c35d345ddd9b0991cfbac98f9776c0
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-21 14:42:06 +0200

    ejercicio4_51

--- src/boletin4_5.java
+++ src/boletin4_5.java
@@ -5,12 +5,18 @@
  */
 public class boletin4_5 {
     public static void main(String[] args) {
-        System.out.println("Cuenta atras: ");
+        System.out.println("Cuenta: ");
         int i;
-    for (i = 10; i >= 0; i--){
+    for (i = 0; i <= 10; i++){
         System.out.println(i);
 
         }
-        System.out.println("BOOM");
+
+        System.out.println("PAW");
+    for (i = 10; i>= 0; i--){
+        System.out.println(i);
     }
+        System.out.println("POW");
+    }
+
 }
