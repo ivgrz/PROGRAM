36814eb6d3e9bf366b368fc380e9623b8bb46013
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-23 10:07:41 +0200

    bol4_51

--- src/boletin4_5.java
+++ src/boletin4_5.java
@@ -5,18 +5,24 @@
  */
 public class boletin4_5 {
     public static void main(String[] args) {
-        System.out.println("Cuenta: ");
+ /*       System.out.println("Cuenta: ");
         int i;
-    for (i = 0; i <= 10; i++){
-        System.out.println(i);
+        for (i = 0; i <= 10; i++) {
+            System.out.println(i);
 
         }
 
         System.out.println("PAW");
-    for (i = 10; i>= 0; i--){
-        System.out.println(i);
-    }
+        for (i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
         System.out.println("POW");
+*/
+        int z = 10;
+        while (z != 0) {
+            System.out.println(z);
+            z--;
+        }
     }
 
 }
