77efbd4306d8320a1aad7ed026310f1361008e84
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-23 09:28:28 +0200

    ejerciciop1

---
+++ src/ejerciciop1.java
@@ -0,0 +1,18 @@
+/**
+ * Hallar los multiplos de 7 menores que 100
+ * @version 1.1
+ * @author ivan
+ */
+public class ejerciciop1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int numero = 7;
+        for (;i <= 14; i++) {
+            System.out.println((numero * i));
+        }
+
+
+
+
+    }
+}
