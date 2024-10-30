1bef1e9e0e918c1b0988b5b4a556f76516ad8888
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-30 14:03:35 +0100

    cuentaletra1

--- src/Cuentalet.java
+++ src/Cuentalet.java
@@ -1,16 +1,18 @@
 /**
  * Prueba y documentacion de funciones
+ *
  * @author ivan
  * @version 1.1
  */
 public class Cuentalet {
     /**
      * Metodo principal, entrada de la aplicacion
+     *
      * @param args
      */
-        public static void main(String[] args) {
-            cuentaletras("hola","mundo");
-        }
+    public static void main(String[] args) {
+        cuentaletras("hola", "mundo");
+    }
 
     /**
      * Cuenta la totalidad de las letras de dos palabras
@@ -19,10 +21,10 @@ public class Cuentalet {
      * @param b palbra a contar caracteres
      * @return numero de letras totales
      */
-        static int cuentaletras(String a, String b){
-            int total =a.length() + b.length();
-            return 0;
-        }
+    static int cuentaletras(String a, String b) {
+        int total = a.length() + b.length();
+        return 0;
+    }
 
 }
 
