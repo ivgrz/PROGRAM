66f7fe99739040d073494b94229de778c082acc3
Ivan <igutierrezcontreras@danielcastelao.org>
2024-11-06 14:19:51 +0100

    Fibonacci2dometodo(sin funcion)

---
+++ src/Fibonacci2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci2 {
+    public static void main(String[] args) {
+        int n1, n2, ns;
+    Scanner sc = new Scanner(System.in);
+        System.out.println("introduce el limite para la sucesion: ");
+        int l = sc.nextInt();
+
+        n1 = 2;
+        n2 = 3;
+
+        for (int i = 0; i < l; i++){
+            ns = n1 + n2;
+            System.out.println(ns);
+
+            n1 = n2;
+            n2 = ns;
+
+        }
+
+    }
+
+
+
+}
