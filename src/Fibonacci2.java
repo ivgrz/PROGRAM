89daca7b8c46649fed3d3e4b64db5ccca65ec652
Ivan <igutierrezcontreras@danielcastelao.org>
2024-11-06 14:20:43 +0100

    Fibonacci2dometodo_sin_funcion

--- src/Fibonacci2.java
+++ src/Fibonacci2.java
@@ -5,12 +5,12 @@ public class Fibonacci2 {
         int n1, n2, ns;
     Scanner sc = new Scanner(System.in);
         System.out.println("introduce el limite para la sucesion: ");
-        int l = sc.nextInt();
+        int limite = sc.nextInt();
 
         n1 = 2;
         n2 = 3;
 
-        for (int i = 0; i < l; i++){
+        for (int i = 0; i < limite; i++){
             ns = n1 + n2;
             System.out.println(ns);
 
