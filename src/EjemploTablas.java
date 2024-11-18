1f35955a58cb4c7f97b40dd06970fb0a34fa712e
Ivan <igutierrezcontreras@danielcastelao.org>
2024-11-18 14:25:02 +0100

    Ejemtablas

---
+++ src/EjemploTablas.java
@@ -0,0 +1,21 @@
+public class EjemploTablas {
+    public static void main(String[] args) {
+        int[] edades;
+        edades = new int[5];
+        edades[0] = 31;
+        edades[1] = 23;
+        edades[2] = 56;
+        edades[3] = 12;
+        edades[4] = 32;
+        System.out.println(edades[4]);
+        String[] nombres = {"Manuel", "Jose", "Rosa", "Ana"};
+        System.out.println(nombres[3]);
+
+        int minimo = edades[0];
+        for (int i = 1; i < edades.length; i++){
+           if (minimo > edades[i]) minimo = edades[i];
+            System.out.println("el valor mismo de edades es: "+ minimo);
+        }
+
+    }
+}
