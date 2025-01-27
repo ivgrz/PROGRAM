0711d8b19eff90f42c354981b6516e57a4055c13
Ivan <igutierrezcontreras@danielcastelao.org>
2025-01-27 12:58:42 +0100

    Boletin8.3ej1

---
+++ src/Boletin8_3/Ejercicio1.java
@@ -0,0 +1,32 @@
+package Boletin8_3;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String secuencia = "";
+        int num = 0;
+
+    }
+
+    public static String dividida(String palabra, int sec){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa una palabra:");
+
+        palabra = sc.nextLine();
+
+
+        do {
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
