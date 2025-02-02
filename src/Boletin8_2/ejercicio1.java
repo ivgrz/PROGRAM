511d3b823c407c4f2cabce3b7c20c9e8952c0eb5
IvnGC <igutierrezcontreras@danielcastelao.org>
2025-02-02 02:23:45 +0100

    avaneejer1

---
+++ src/Boletin8_2/ejercicio1.java
@@ -0,0 +1,27 @@
+package Boletin8_2;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe una cadena de caracteres: ");
+        String cadena = sc.nextLine();
+        int n = 0;
+        dosprimeroscaract(cadena, n);
+
+
+    }
+
+public static void dosprimeroscaract(String cadena, int n){
+
+        for (int i = 0; i < 1; i++){
+            String subcadena = cadena.substring(0,2);
+            System.out.println(subcadena);
+
+        }
+public static void
+}
+
+
+}
