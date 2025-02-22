8423731f1f020e0e56670eaa1c417c31374a05bd
IvnGC <igutierrezcontreras@danielcastelao.org>
2025-02-22 15:28:25 +0100

    boletin9_Libroterminado

--- POO/src/Boletin9/Librosalida.java
+++ POO/src/Boletin9/Librosalida.java
@@ -1,4 +1,21 @@
 package Boletin9;
 
+/**
+ * @author ivan
+ * @version 1.0
+ */
 public class Librosalida {
+    public static void main(String[] args) {
+    //Usando el constructor por defecto
+        System.out.println("Libro hecho con el constructor por defecto: ");
+        Libro libro1 = new Libro();
+
+        libro1.mostrar();
+
+    //Usando el constructor con parametros
+        System.out.println("Libro hecho con el segundo constructor: ");
+        Libro libro2 = new Libro("La ciudad y los perros", "Mario Vargas Llosa", 1963,(short)448);
+        libro2.mostrar();
+
+    }
 }
