31c69efc9db08f0e222065413dd25f74764ff68c
karly <kalbarransanchez@danielcastelao.org>
2025-02-27 12:51:37 +0100

    Animalv1

---
+++ Clase_Abstracta/src/Animal.java
@@ -0,0 +1,34 @@
+public class Animal {
+    private String Nombre;
+    private int edad;
+
+    public Animal(){
+
+
+    }
+    public String comer(){
+
+    }
+    public String dormir(){
+
+
+    }
+    public String desplazar(){
+
+
+    }
+    public String getNombre(){
+       return Nombre;
+    }
+    public void setNombre(){
+
+    }
+    public int getEdad(){
+    return  edad;
+    }
+    public void setEdad(){
+
+    }
+
+}
+
