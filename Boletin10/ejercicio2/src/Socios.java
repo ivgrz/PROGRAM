9f3ca58ced75cdf26bc72114ce292bd601ddbdf6
karly <kalbarransanchez@danielcastelao.org>
2025-03-10 13:50:21 +0100

    Boletin10ejer2

---
+++ Boletin10/ejercicio2/src/Socios.java
@@ -0,0 +1,10 @@
+public class Socios {
+    private String DNI;
+    private String Nombre;
+    private String direccion;
+    private int fechaalta;
+    private int cuotamensual;
+
+
+}
+
---
+++ Boletin10/ejercicio2/src/Trabajadores.java
@@ -0,0 +1,9 @@
+public class Trabajadores {
+
+
+private String DNI;
+private String nombre;
+private int fechaingreso;
+
+
+}
