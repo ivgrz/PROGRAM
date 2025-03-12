b8de8b10d4820d8683ba54cd56e790f30cad90a2
karly <kalbarransanchez@danielcastelao.org>
2025-03-12 09:54:07 +0100

    ejer2_socios

--- Boletin10/ejercicio2/src/Socios.java
+++ Boletin10/ejercicio2/src/Socios.java
@@ -1,10 +1,26 @@
-public class Socios {
-    private String DNI;
-    private String Nombre;
+import DataHora.Data;
+public class Socios extends Miembroasociacion implements CalculoGasto {
+
     private String direccion;
+    private String Provincia;
     private int fechaalta;
-    private int cuotamensual;
+    private double cuotamensual;
+    private Data DataAlta;
+
+    public Socios(String nombre, String dni, String direccion, String provincia){
+        super (dni, nombre);
+        this.direccion = direccion;
+        this.Provincia = provincia;
+
+    }
+    public double gastosIngresos(){
+        return
+    }
 
 
+    public String aCadea (){
+        String aux = ", con direccion" + direccion + ", en la provincia "
+                return super
+    }
 }
 
