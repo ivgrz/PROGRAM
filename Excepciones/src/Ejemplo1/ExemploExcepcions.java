a7c7f6cf0bdcf7f66d88b34ffdfc2b537cf9f74d
Ivan <igutierrezcontreras@danielcastelao.org>
2025-03-31 13:11:22 +0200

    Excepcions_Exemplo_1

--- Excepciones/src/Ejemplo1/ExemploExcepcions.java
+++ Excepciones/src/Ejemplo1/ExemploExcepcions.java
@@ -6,19 +6,22 @@ package Ejemplo1;
  */
 public class ExemploExcepcions {
     /**
-     * Exemplo de excepcións
+     *  Metodo principal que ejecuta el ejemplo de manejo de excepciones.
      * @param args
      */
     public static void main(String[] args) {
-        int a,b,c;
+        int a,b,c; //Variables para la division
         a = 0;
         b = 0;
-        try{
+        try{ //Bloque de codigo que puede lanzar una excepcion
         c = a/b;
-        } catch (ArithmeticException erro){
+        } catch (ArithmeticException erro){ //Captura la excepcion
             System.out.println("Parece que b è 0 ,non se pode dividir entre 0");
             System.out.println(erro.getMessage());//Muestra y recorre el mensaje de error
 
+        } catch (Exception erro2){
+            System.out.println("Volvio a fallar bobin");
+            System.out.println(erro2.getMessage());
         }
     }
 }
