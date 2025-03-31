bf2cfc4e8eb904ce0f44803b0cc09525b498f36e
Ivan <igutierrezcontreras@danielcastelao.org>
2025-03-31 13:23:07 +0200

    Excepcions_Exemplo_2

--- Excepciones/src/Ejemplo1/ExemploExcepcions.java
+++ Excepciones/src/Ejemplo1/ExemploExcepcions.java
@@ -1,5 +1,7 @@
 package Ejemplo1;
 
+import java.io.FileNotFoundException;
+
 /**
  * @author ivan
  * @version 1.0
@@ -15,13 +17,17 @@ public class ExemploExcepcions {
         b = 0;
         try{ //Bloque de codigo que puede lanzar una excepcion
         c = a/b;
-        } catch (ArithmeticException erro){ //Captura la excepcion
+        } catch (ArithmeticException |IllegalArgumentException erro){ //Captura la excepcion y la almacena en la variable erro
             System.out.println("Parece que b è 0 ,non se pode dividir entre 0");
             System.out.println(erro.getMessage());//Muestra y recorre el mensaje de error
 
-        } catch (Exception erro2){
-            System.out.println("Volvio a fallar bobin");
+        }   finally {
+            System.out.println("O bloque finally executase sempre");
+        } /*catch (Exception erro2){
             System.out.println(erro2.getMessage());
+
         }
+        */
+
     }
 }
