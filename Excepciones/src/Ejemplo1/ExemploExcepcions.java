8696e954b710428d8679dc904aefdb95015e6a48
Ivan <igutierrezcontreras@danielcastelao.org>
2025-03-31 13:00:26 +0200

    Excepcions_Exemplo

--- Excepciones/src/Ejemplo1/ExemploExcepcions.java
+++ Excepciones/src/Ejemplo1/ExemploExcepcions.java
@@ -1,14 +1,24 @@
 package Ejemplo1;
 
+/**
+ * @author ivan
+ * @version 1.0
+ */
 public class ExemploExcepcions {
+    /**
+     * Exemplo de excepcións
+     * @param args
+     */
     public static void main(String[] args) {
         int a,b,c;
         a = 0;
         b = 0;
         try{
         c = a/b;
-        } catch (arithmeticException erro){
-            System.out.println("Parece que b e 0 ,non se pode dividir entre 0");
+        } catch (ArithmeticException erro){
+            System.out.println("Parece que b è 0 ,non se pode dividir entre 0");
+            System.out.println(erro.getMessage());//Muestra y recorre el mensaje de error
+
         }
     }
 }
