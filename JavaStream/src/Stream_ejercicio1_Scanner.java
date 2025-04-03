62c7f6fd78a7977ebddd00a00bdd4c65ac8b83c9
Ivan <igutierrezcontreras@danielcastelao.org>
2025-04-03 14:41:46 +0200

    JavaStream_sexto_commit

--- JavaStream/src/Stream_ejercicio1_Scanner.java
+++ JavaStream/src/Stream_ejercicio1_Scanner.java
@@ -2,36 +2,35 @@ import java.io.*;
 import java.util.Scanner;
 // Ejercicio 1: Leer un fichero de texto que contiene números enteros separados por espacios y calcular la suma y el promedio de esos números.
 public class Stream_ejercicio1_Scanner {
-    public Stream_ejercicio1_Scanner(String RutaFicheriro){
-        BufferedReader entrada = null;
+    public Stream_ejercicio1_Scanner(String RutaFicheiro){
+        Scanner entrada = null;
         try{
             int suma = 0, cantidad = 0;
-            entrada = new BufferedReader(new FileReader(RutaFicheriro));
-            String linea = entrada.readLine();
-            while (linea != null) {
+
+            entrada = new Scanner(new File(RutaFicheiro));
+
+            while (entrada.hasNextLine()) {
+                String linea = entrada.nextLine();
                 Scanner sc = new Scanner(linea);
-            while (sc.hasNext()){
+            while (sc.hasNextInt()){
             suma = suma + sc.nextInt();
                 cantidad++;
             }
-            linea = entrada.readLine();
+            sc.close();
             }
             System.out.println("Suma: " + suma);
             System.out.println("Promedio: " + (double) suma / cantidad);
             }catch (FileNotFoundException e){
             System.out.println("No se encuentra el fichero" + e.getMessage());
-        } catch (IOException e){
-            System.out.println("Error de entrada/salida" + e.getMessage());
         }
         finally {
             if (entrada != null){
-                try {
-                    entrada.close();
-                }catch (IOException e){
-                    System.out.println("Error al cerrar el archivo" + e.getMessage());
-                }
+
+                entrada.close();
+
+            }
             }
         }
 
     }
-}
+
--- JavaStream/src/Stream_ejercicio1_main.java
+++ JavaStream/src/Stream_ejercicio1_main.java
@@ -1,5 +1,7 @@
 public class Stream_ejercicio1_main {
     public static void main(String[] args) {
-        new Stream_ejercicio1("src/Fichero_ejercicio1.txt");
+      //  new Stream_ejercicio1("JavaStream/src/Fichero_ejercicio1.txt");
+        new Stream_ejercicio1_Scanner("/home/dam/Escritorio/PROGIV/clasesprogram/JavaStream/src/Fichero_ejercicio1");
+    //copiar el path de referencia entero
     }
 }
