8826386de6647f3a4ac10c4b6a7820e69baa3af6
Ivan <igutierrezcontreras@danielcastelao.org>
2025-04-10 13:38:11 +0200

    Boletin12_tercer_Commit

---
+++ Boletin12/src/TareasxHacer.java
@@ -0,0 +1,12 @@
+/*
+Crear unha aplicación para anotar tarefas para facer. A tarefa terá unha data, hora, duración, Nome da tarefa, descripción,  estado (feita, non feita). Para iso crear unha clase Tarefa que manexe os datos relacionados coa tarefa.  O usuario poderá facer as seguintes operacións:
+Agregar unha nova tarefa.
+Borrar unha tarefa.
+Modificar unha tarefa.
+Listar o listado de tarefas.
+As tarefas se gardarán nun ficheiro binario chamado tarefas.dat.
+
+*/
+
+public class TareasxHacer {
+}
