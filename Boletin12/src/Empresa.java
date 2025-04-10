4633120f4372a92f4bde5ac89052356a85e49977
Ivan <igutierrezcontreras@danielcastelao.org>
2025-04-10 13:09:45 +0200

    Boletin12_primer_Commit

---
+++ Boletin12/src/Empresa.java
@@ -0,0 +1,15 @@
+/*
+Crea a aplicación que premita gardar e recuperar os datos dos clentes dunha empresa. Para o cal, defina a clase Cliente, que teña os atributos:
+id, identificador de cliente
+nome
+teléfono
+	Os obxectos Cliente se recollerán nunha táboa .
+	A aplicación terá un menú que posibilitará as seguintes opcións:
+Engadir novo cliente
+Modificar datos
+Dar de baixa clientes.
+Listar os clientes.
+	A información se gardará nun ficheiro binario, que se cargará en memoria o iniciar o programa e se gardará en disco, actualizado o rematar.
+*/
+public class Empresa {
+}
---
+++ Boletin12/src/Notas.java
@@ -0,0 +1,26 @@
+/*
+Crear un programa xestor de notas persoais, que permita ao usuario gardar e consultar notas.
+O usuario pode engadir unha nova nota (texto libre).
+
+
+As notas gárdanse nun ficheiro de texto (notas.txt), unha por liña.
+
+
+O programa pode listar todas as notas gardadas.
+
+
+O usuario pode buscar notas que conteñan unha palabra clave.
+
+Ler un ficheiro de texto e contar cantas veces aparece cada palabra.
+Solicita ao usuario o nome dun ficheiro .txt.
+
+
+Mostra a frecuencia de cada palabra (ignorando maiúsculas/minúsculas e signos de puntuación).
+
+
+Gárdase un resumo nun novo ficheiro resumo_palabras.txt.
+
+
+ */
+public class Notas {
+}
